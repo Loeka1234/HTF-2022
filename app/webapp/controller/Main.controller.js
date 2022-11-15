3c3fcacce8c327c83d2d48e76a0fb96988e740e9
Loeka Lievens <loekalievens@hotmail.com>
2022-11-15 12:47:00 +0100

    iDifference

--- app/webapp/controller/Main.controller.js
+++ app/webapp/controller/Main.controller.js
@@ -369,9 +369,11 @@ sap.ui.define(
           .getResourceBundle()
           .getText("unitWeek")})`;
 
-        // BASIC
+        // BASIC: Done (Same as with month)
         // Calculate the difference between the total consumption of this week and total consumption of last week
-        const iDifference = 0;
+        const iDifference =
+          oFlowModel.totalConsumptionWeek -
+          (oFlowModel.totalConsumptionWeekPast ?? 0);
 
         const bState = iDifference > 0;
         const sDifference = `${iDifference.toFixed(2)} (${this.getView()
@@ -420,9 +422,11 @@ sap.ui.define(
           .getResourceBundle()
           .getText("unitDay")})`;
 
-        // BASIC
+        // BASIC: Done (Same as with month)
         // Calculate the difference between the total consumption of today and total consumption of yesterday
-        const iDifference = 0;
+        const iDifference =
+          oFlowModel.totalConsumptionToday -
+          (oFlowModel.totalConsumptionTodayPast ?? 0);
 
         const bState = iDifference > 0;
         const sDifference = `${iDifference.toFixed(2)} (${this.getView()
