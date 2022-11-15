60a07eab55cb265f431f42a58b22a43ee5e75493
Loeka Lievens <loekalievens@hotmail.com>
2022-11-15 10:46:26 +0100

    add to existing dataset

--- srv/service.js
+++ srv/service.js
@@ -1,82 +1,101 @@
-const cds = require('@sap/cds');
-const mqtt = require('mqtt');
+const cds = require("@sap/cds");
+const mqtt = require("mqtt");
 
 let aDataSet = [];
 
 // Credentials mqtt broker
 var oOptions = {
-  host: 'ac24c670632142bab0a422606038f608.s1.eu.hivemq.cloud',
+  host: "ac24c670632142bab0a422606038f608.s1.eu.hivemq.cloud",
   port: 8883,
-  protocol: 'mqtts',
-  username: 'flexso',
-  password: 'flexsohtf'
-}
+  protocol: "mqtts",
+  username: "flexso",
+  password: "flexsohtf",
+};
 
 // initialize the MQTT client
 var client = mqtt.connect(oOptions);
 
 // setup the callbacks
-client.on('connect', function () {
-  console.log('Connected');
+client.on("connect", function () {
+  console.log("Connected");
 });
 
-client.on('error', function (error) {
+client.on("error", function (error) {
   console.log(error);
 });
 
-client.on('message', function (topic, message) {
+let skip = false;
+
+client.on("message", function (topic, message) {
   let jsonS = message.toString();
   let obj = JSON.parse(jsonS);
   obj.datetime = new Date(obj.datetime);
 
   // BASIC
   // Incoming logs should be added to the existing dataset (aDataset)
+  console.log(obj);
+  cds.insert(obj);
 
   // ADVANCED
   // We only want to register useful data, so negate consecutive flows with zero debit.
   // We do want to know when the flow starts and stops though.
-  // Example: 
-    // Incoming data: 0 - 1 - 2 - 1 - 0 - 0 - 0 - 0 - 0 - 0 - 1 - 2 - 3
-    // Result:        0 - 1 - 2 - 1 - 0 -               - 0 - 1 - 2 - 3
+  // Example:
+  // Incoming data: 0 - 1 - 2 - 1 - 0 - 0 - 0 - 0 - 0 - 0 - 1 - 2 - 3
+  // Result:        0 - 1 - 2 - 1 - 0 -               - 0 - 1 - 2 - 3
+  // if (obj.flow > 0) {
+  //   if (skip) return;
+
+  //   skip = true;
+  // }
+  // cds.insert(obj);
 });
 
 // BASIC
 // Subscribe to topic '/flowMeter'
+client.subscribe("/flowMeter");
 
 // OPTIONAL: Only use when IoT device is not running
 // getTestData();
 
 module.exports = (srv) => {
-  srv.on('READ', 'FlowStream', async (req, res) => {
-    let aMockResults = await SELECT.from('FlowStreamService.FlowStream', () => { '*' });
+  srv.on("READ", "FlowStream", async (req, res) => {
+    let aMockResults = await SELECT.from("FlowStreamService.FlowStream", () => {
+      "*";
+    });
     let aMockAndRealtimeData = [...aMockResults, ...aDataSet];
     return aMockAndRealtimeData.sort((a, b) => a.datetime > b.datetime);
   });
 
-  srv.on('READ', 'FlowHint', async (req, res) => {
-    let aQuotes = await SELECT.from('FlowStreamService.FlowHint', () => { '*' });
+  srv.on("READ", "FlowHint", async (req, res) => {
+    let aQuotes = await SELECT.from("FlowStreamService.FlowHint", () => {
+      "*";
+    });
     return aQuotes;
   });
 
-  srv.on('READ', 'GandalfQuote', async (req, res) => {
-    let aQuotes = await SELECT.from('FlowStreamService.GandalfQuote', () => { '*' });
+  srv.on("READ", "GandalfQuote", async (req, res) => {
+    let aQuotes = await SELECT.from("FlowStreamService.GandalfQuote", () => {
+      "*";
+    });
     return aQuotes;
   });
-}
+};
 
-function getTestData(){
+function getTestData() {
   // Create testrecord
   const oTestRecord = {
     flow: _getRndInteger(8, 18), //  low 8-10; normal 10-14, high 14-16 current
     datetime: new Date(),
-    descr: "flow in L/min"
-  }
+    descr: "flow in L/min",
+  };
   // Add to dataset
   aDataSet.push(oTestRecord);
   // Repeat every 2 seconds
-  setTimeout(function(){ getTestData() }, 2000)
+  setTimeout(function () {
+    getTestData();
+  }, 2000);
 }
 
 function _getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
